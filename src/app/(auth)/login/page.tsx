b119afea8ca2b9c41f8f0027b5c47883d5b8ce66
Alexis <desert5@outlook.es>
2025-05-11 00:10:44 -0400

    feat: Update LoginPage layout and styling; add background image and centered content

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,15 @@
 export default function LoginPage() {
-  return <div className="flex">asddas</div>;
+  return (
+    <div
+      className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center px-2 md:px-0"
+      style={{
+        backgroundImage:
+          "linear-gradient(0deg,rgba(0,0,0,.52) 0%,rgba(0,0,0,.52) 100%),url('/assets/login/login-bg.jpg')",
+      }}
+    >
+      <div className="flex max-w-[704px] w-full py-[36px] px-[128px] flex-col justify-center items-center gap-6 rounded-[24px] bg-white">
+        En desarrollo 👷‍♂️
+      </div>
+    </div>
+  );
 }
