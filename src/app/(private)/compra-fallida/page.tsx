c5f694c59716193263eb464467c731ae4f317d82
franmadev <f.marambio@live.cl>
2025-05-13 20:40:59 -0400

    fix compra-fallida

---
+++ src/app/(private)/compra-fallida/page.tsx
@@ -0,0 +1,9 @@
+export default function CompraFallidaPage() {
+  return (
+    <div className="flex flex-col h-full">
+      <div className="flex-grow flex items-center justify-center">
+        <h1 className="text-2xl font-bold">Página Compra Fallida ❌</h1>
+      </div>
+    </div>
+  );
+}
