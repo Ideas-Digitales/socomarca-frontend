a8682778033a354f70d8560f849f51a32efed984
Alexis <desert5@outlook.es>
2025-05-22 23:21:48 -0400

    refactor: update layout and product card components for improved responsiveness and price handling

--- src/app/(administrador)/admin/page.tsx
+++ src/app/(administrador)/admin/page.tsx
@@ -15,20 +15,20 @@ export default function AdministradorPage() {
   });
 
   return (
-    <div className="bg-white flex py-7 px-12 items-start gap-7">
+    <div className="bg-white flex flex-col md:flex-row py-7 px-12 items-start gap-7">
       <div className="flex flex-col justify-between items-center h-full">
         <div className="flex h-full items-center">
           <DayPickerComponent />
         </div>
       </div>
       {/* Compradores */}
-      <div className="flex flex-col items-start gap-4 flex-1-0-0 max-w-[552px] min-w-[552px]">
-        <div className="flex items-center gap-3 w-full">
-          <button className="w-1/3 bg-gray-100 flex justify-between items-start p-[10px] h-10 text-gray-500 text-sm">
+      <div className="flex flex-col items-start gap-4 flex-1-0-0 md:max-w-[552px] md:min-w-[552px] md:bg-red-200">
+        <div className="flex flex-col md:flex-row items-center gap-3 w-full">
+          <button className="w-1/3 bg-gray-100 flex justify-between items-start p-[10px] h-10 text-gray-500 text-md">
             Montos
             <ChevronDownIcon width={20} height={20} />
           </button>
-          <button className="w-1/3 bg-gray-100 flex justify-between items-start p-[10px] h-10 text-gray-500 text-sm">
+          <button className="w-1/3 bg-gray-100 flex justify-between items-start p-[10px] h-10 text-gray-500 text-md">
             Cliente
             <MagnifyingGlassIcon width={20} height={20} />
           </button>
@@ -39,11 +39,11 @@ export default function AdministradorPage() {
         <div className="flex flex-col gap-4 py-[14px] px-5 rounded-[6px] border-gray-100 border-[1px] border-solid w-full">
           <div className="flex items-start gap-4">
             <div className="text-lime-500 w-1/2">
-              <p className="text-sm font-medium">Total de compradores</p>
+              <p className="text-md font-medium">Total de compradores</p>
               <h5 className="text-lg font-bold">850</h5>
             </div>
             <div className="text-gray-500 w-1/2">
-              <p className="text-sm font-medium">Total de compradores</p>
+              <p className="text-md font-medium">Total de compradores</p>
               <h5 className="text-lg font-bold">850</h5>
             </div>
           </div>
